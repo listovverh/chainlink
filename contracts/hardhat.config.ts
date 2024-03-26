b6bad9275326ef42e2e91669af7364cc22896469
Rens Rooimans <github@rensrooimans.nl>
2024-03-26 10:27:19 +0100

    Migrate waffle -> hardhat-chai-matchers

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,6 +1,6 @@
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-etherscan'
-import '@nomiclabs/hardhat-waffle'
+import '@nomicfoundation/hardhat-chai-matchers'
 import '@openzeppelin/hardhat-upgrades'
 import '@typechain/hardhat'
 import 'hardhat-abi-exporter'
